66b53db0287f3b5d5827a36a8cf4f57e22e71fc1
Deepak KC <deepak.curator@gmail.com>
2024-01-25 22:51:34 +0200

    gitignore created

---
+++ opi/lect1.php
@@ -0,0 +1,38 @@
+<?php
+// This is your first script in PHP
+echo "Hello, world!";
+
+// PHP and HTML can be used together
+echo "<h1>Welcome to PHP Programming!</h1>";
+
+// PHP is case sensitive
+$myVariable = "Hello";
+//$myvariable is not the same as $myVariable
+//echo $myvariable; // This will give an error
+
+// Using and printing a string with a variable
+$myString = "PHP Programming";
+echo $myString;
+
+// Echoing quotes in PHP
+echo "He said, \"Learn PHP. It's fun!\"";
+
+// Echoing variables
+$var1 = "Hello";
+$var2 = "World";
+echo $var1 . " " . $var2; 
+
+// Echoing variables and text
+echo "This is " . $var1 . " from the " . $var2; 
+
+// Printing multiple strings
+echo "Hello, ", "World!", " How", " are", " you?";
+
+// Printing in a new line (in a browser):
+echo "Hello, World!<br>";
+echo "How are you?<br>";
+
+//Printing in a new line (in a command-line interface)
+echo "Hello, World!\n";
+echo "How are you?\n";
+?>
